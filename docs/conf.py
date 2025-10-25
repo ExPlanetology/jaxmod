c9c024255f792eaa0f83ea5e3bd16006e92049ea
Dan J Bower <djbower@users.noreply.github.com>
2025-10-25 11:10:53 +0200

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = "2025, Dan J. Bower"
 author = "Dan J. Bower"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.2"
+release = "0.2.3"
 
 
 # -- General configuration ---------------------------------------------------
--- jaxmod/__init__.py
+++ jaxmod/__init__.py
@@ -21,7 +21,7 @@ import logging
 import jax
 import numpy as np
 
-__version__: str = "0.2.2"
+__version__: str = "0.2.3"
 
 jax.config.update("jax_enable_x64", True)
 
